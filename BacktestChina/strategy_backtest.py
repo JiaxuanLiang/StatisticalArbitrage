cc2c788d3106efa76dda52740ad39bd778a7edc3
Jiaxuan Liang <jiaxuanliang.w@gmail.com>
2022-07-18 19:13:14 -0400

    files updates and a summary commit

--- BacktestChina/strategy_backtest.py
+++ BacktestChina/strategy_backtest.py
@@ -10,12 +10,17 @@ Change Activity:  7/17/22
 --------------------------------------------------
 """
 
+# import packages
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
 
 
+# calculate position based on signals
+# here the position equals to the signal of the previous day
+# suppose position is completed with the close price of previous day
+# and the profit & loss is calculated in the day
 def calculate_position(raw_data, *args):
     rf = args[0]
     k = args[1]
@@ -24,20 +29,24 @@ def calculate_position(raw_data, *args):
                                                              '前收盘价': 'lastClosePrice'})
     data['date'] = data.date.apply(np.datetime64)
     data = data.set_index('date')
-    data['ret'] = np.log(data.closePrice).diff(1)
+    data['ret'] = np.log(data.closePrice/data.lastClosePrice)
+    # the condition for buy hold strategy
     data['long_signal'] = data.ret.rolling('7d').apply(lambda s: s[-1]-np.var(s)/2 > rf)*1
     data['out_signal'] = (np.exp(data.ret) > (1+k)*np.exp(rf/252))*1
     data['signal'] = np.maximum(data.long_signal-data.out_signal, 0)
-    data['position'] = data.signal.shift(1)
+    data['position'] = data.signal.shift(1).fillna(0)
     return data
 
 
+# calculate pnl for each stock
 def calculate_pnl(data, cost):
     day_pnl = data.position*(data.closePrice-data.lastClosePrice)
+    # if today's signal indicate a different position for tomorrow different from today
+    # transaction happens at the close price of today
     trans_cost = (data.signal != data.position)*cost
     data['day_pnl'] = day_pnl
     data['trans_cost'] = trans_cost
-    data['pnl_with_trans'] = data['day_pnl']-data['trans_cost']
+    data['pnl_with_trans'] = data['day_pnl']-data['trans_cost']*data['closePrice']
     return data
 
 
@@ -49,12 +58,12 @@ rf = np.log(1+0.02)
 k = 1
 transaction_cost = 0.0006
 
-pnl_with_trans = {}
 for file in files:
-    file_name = file.split('.')[0]
+    file_name = file.split('.')[0]  # the stock id
     raw_data = pd.read_csv(path+'/'+file, skiprows=1, encoding='GBK')
     data = calculate_position(raw_data, rf, k)
     port = calculate_pnl(data, transaction_cost)
+    # data is cleaned so nan will happen only when there is no transaction
     pnl_accumulated = port.copy().pnl_with_trans.fillna(0)
 
     for i in range(1, len(pnl_accumulated)):
@@ -62,20 +71,24 @@ for file in files:
         last_pnl_accumulated = pnl_accumulated[i-1]*reinvest_coef
         pnl_accumulated[i] = last_pnl_accumulated + pnl_accumulated[i]
 
-    # contract
-    pnl_holding_stock = port.closePrice-port.closePrice[0]*np.exp((port.index-port.index[0]).days/252*rf)
-    pnl_with_trans[file_name] = pd.Series(pnl_accumulated, index=port.index.values)
+    trans_dates = port[port.trans_cost != 0].index
+    in_date = trans_dates[::2]
+    in_accumu_pnl = pnl_accumulated[in_date]
+    out_date = trans_dates[1::2]
+    out_accumu_pnl = pnl_accumulated[out_date]
 
-    figure = plt.figure(figsize=(8, 6))
+    # by contract, keep holding the stock at the first day financed by the money account
+    pnl_holding_stock = port.closePrice-port.closePrice[0]*np.exp((port.index-port.index[0]).days/252*rf)
+
+    # make plots
+    figure = plt.figure(figsize=(20, 8))
     plt.plot(port.index.values, pnl_accumulated, label='strategy pnl')
     plt.plot(port.index.values, pnl_holding_stock.values, label='stock holding pnl')
+    plt.scatter(in_date.values, in_accumu_pnl.values, c='green', marker='.', alpha=0.4, label='long point')
+    plt.scatter(out_date.values, out_accumu_pnl.values, c='red', marker='.', alpha=0.4, label='out point')
     plt.xlabel('date')
     plt.ylabel('profit/Chinese yen')
-    plt.xticks(rotation=45)
+    plt.xticks(rotation=30)
     plt.title(file_name+' accumulated pnl')
     plt.legend()
     plt.savefig(file_name+'.png')
-
-pnls_with_trans = pd.DataFrame(pnl_with_trans).fillna(0)
-pnl_accumulated = pnls_with_trans.copy()
-
